5362ff16b9193b7d5f5a69794dc4136ab1a1c43f
Roy Rodríguez Segura <103152684+Roy-ARS@users.noreply.github.com>
2025-01-31 10:23:58 -0600

    Update divisonEuclideana.py

--- divisonEuclideana.py
+++ divisonEuclideana.py
@@ -1,4 +1,6 @@
-#Realiza la división euclideana vista en el curso de matemáticas y muestra el procedimiento paso por paso
+#Realiza la división euclideana vista en el curso de matemáticas
+#muestra el procedimiento paso por paso
+#muestra el Mínimo Común Múltiplo de los números dados
 def main():
     a = int(input("Define a: "))
     b = int(input("Define b: "))
